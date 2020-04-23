e26371585102d709ba75b2d72f40df8da6605ea9
Sami <49096040+karnBaba@users.noreply.github.com>
2020-04-24 00:47:11 +0530

    Add files via upload

---
+++ build/precache-manifest.a7959014517f0317b14ac7b3e545f620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6b62b779716086a1f1f24a8f5c38f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb46a67c41ddeb73e38",
+    "url": "/static/css/main.2481a2ea.chunk.css"
+  },
+  {
+    "revision": "c5a61c185b4acad61f4a",
+    "url": "/static/js/2.ff892cd6.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.ff892cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb46a67c41ddeb73e38",
+    "url": "/static/js/main.8042e1cc.chunk.js"
+  },
+  {
+    "revision": "7279276d6d44a62d74cb",
+    "url": "/static/js/runtime-main.fa2f5561.js"
+  },
+  {
+    "revision": "1fabef53dd95ef41d8539994503eeade",
+    "url": "/static/media/auth-bg.e1d81745.jpg"
+  }
+]);
---
+++ build/precache-manifest.f6449bfb867e89748f41227fb5b96e21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfd8b4d294eec1c24a2eb30cefaa6f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98040b2392f36985c98e",
+    "url": "/static/css/main.bbd3ed3f.chunk.css"
+  },
+  {
+    "revision": "715a327101fd3cf5fb2b",
+    "url": "/static/js/2.c5f6e682.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.c5f6e682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98040b2392f36985c98e",
+    "url": "/static/js/main.9c9e7486.chunk.js"
+  },
+  {
+    "revision": "7279276d6d44a62d74cb",
+    "url": "/static/js/runtime-main.fa2f5561.js"
+  },
+  {
+    "revision": "1fabef53dd95ef41d8539994503eeade",
+    "url": "/static/media/auth-bg.e1d81745.jpg"
+  }
+]);
