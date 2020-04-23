8c804af8650357bc434fec6073170ec44d4e7409
Sami <49096040+karnBaba@users.noreply.github.com>
2020-04-24 01:28:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ import theme from "../src/utils/theme";
 //import "./styles/index.scss";
 console.log("process env", process.env);
 console.log("server url", process.env.SERVER_URL);
-//const SERVER_URL = "http://localhost:4000/graphql";
+const SERVER_URL = "http://relationshipgossips.herokuapp.com/graphql";
 
 dotenv.config();
-const serverUrl = process.env.REACT_APP_API_URL;
+const serverUrl = process.env.REACT_APP_API_URL || SERVER_URL;
 const client = initApolloClient(serverUrl);
 
 render(
